2532a96b9c5be9dc80b2950a666d4fcf787c573e
moon-dotcom <ansrlwnd88@naver.com>
2024-08-15 16:21:16 +0900

    chain with history

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import sys
 from llm_api import extract_text_from_pdf, get_response
 from file_loader import PDFLoader
 from txt_splitter import SpliterModel
+from prompt import PromptQaChat
+
+### Statefully manage chat history ###
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 def main():
     # PDF file
@@ -22,6 +28,40 @@ def main():
     split_documents = SpliterModel('RecursiveCharacter', docs).split_text()
     
     
+    
+    # Chain with chat history, prompt
+    rag_chain = PromptQaChat(llm, retriever).promptChain()
+    
+    
+    ### Statefully manage chat history ###
+    # ChatMessageHistory가 휘발성이라 main에서 관리
+    # https://python.langchain.com/v0.1/docs/use_cases/question_answering/chat_history/#chain-with-chat-history
+    store = {}
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+    
+    question = input()
+    
+    response = conversational_rag_chain.invoke(
+        {"input": {question}},
+        config={
+            "configurable": {"session_id": "abc123"}
+        },  # constructs a key "abc123" in `store`.
+        )["answer"]
+    print(response)
+    # KeyError('answer')는 langchain오류라고 함
+    
+
     # 인코딩 문제 -> utf-8
     sys.stdout.reconfigure(encoding='utf-8')
     
